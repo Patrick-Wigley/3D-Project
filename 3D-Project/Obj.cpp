e833380402ee86601173b420d6f35876825cb3b2
Patrick <patrickwigley2@gmail.com>
2022-07-24 21:40:23 +0100

    Abstracted VBO, IBO & VAO into dedicated class

---
+++ 3D-Project/Obj.cpp
@@ -0,0 +1,37 @@
+#include "Obj.h"
+
+
+
+Obj::Obj()
+{
+	// Creating Vertex Array Object & Binding
+	glGenVertexArrays(1, &this->vao);
+	glBindVertexArray(vao);
+
+	// Creating Vertex buffer to bind to vao
+	glGenBuffers(1, &this->vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	this->AttachBufferData();
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+
+	glGenBuffers(1, &this->ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), INDICES, GL_STATIC_DRAW);
+
+
+};
+
+
+void Obj::AttachBufferData()
+{
+	glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), VERTICES, GL_STATIC_DRAW);
+}
+
+void Obj::Bind() 
+{
+	glBindVertexArray(this->vao);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ibo);
+}
